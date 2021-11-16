89e9310849125de44cd5d3e2b912abce40d47465
Leithen <leithen113@gmail.com>
2021-11-15 23:01:23 -0700

    Created note saving functionality

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,16 +1,15 @@
 const router = require('express').Router();
 const data = require('../../data/db.json');
+const addNote = require('../../lib/data.js');
 
 router.get('/notes', (req, res) => {
     res.send(data);
 });
-
 router.get('/', (req, res) => {
     res.send('API HERE!');
 });
-
-router.post('', (req, res) => {
-    console.log('Posted!');
+router.post('/notes', (req, res) => {
+    addNote(req.body);
 });
 
 module.exports = router;
