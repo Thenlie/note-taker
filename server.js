16477b6609eee380b3d94ec03ed0afa3be5ff9ea
Leithen <leithen113@gmail.com>
2021-11-16 14:21:23 -0700

    Changed PORT link

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const apiRoute = require('./routes/apiRoutes');
 const htmlRoute = require('./routes/htmlRoutes');
